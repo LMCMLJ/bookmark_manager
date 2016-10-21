48e1a09cae07ebc5b3bd2054d5b3789e089341ea
LMCMLJ <mjcalvey2@gmail.com>
2016-10-21 15:04:53 +0100

    Updates style, working on searching by tags

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,7 @@ require_relative 'models/bookmark'
 class BookmarkManager < Sinatra::Base
 
   database_setup
+  enable :sessions
 
   get "/" do
     @bookmark = Bookmark.new
@@ -23,6 +24,15 @@ class BookmarkManager < Sinatra::Base
     bookmark.create_link(params[:url], params[:title])
     bookmark.add_tags(params[:tags])
     redirect "/"
+  end
+
+  get '/tags/:name' do
+    @search_tag = params[:name]
+    p params[:name]
+    @bookmark = Bookmark.new
+    @links = Link.all
+    @filter_links = @links.select { |link| link.tags.each { |tag| tag.name == @search_tag } }
+    erb :filter_by_tags
   end
 
 
